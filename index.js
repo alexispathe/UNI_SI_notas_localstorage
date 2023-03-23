bcaee638a2fb587d663a75431669ee3e8d7e51ba
Alexis Pathe <alexispatheguillen@gmail.com>
2023-03-23 12:09:29 -0600

    Alexis

--- index.js
+++ index.js
@@ -11,4 +11,27 @@ boton.addEventListener('click', ()=>{
     arreglo.push(areaTexto.value);
     console.log("arreglo", arreglo)
     localStorage.setItem('notas', JSON.stringify(arreglo))
+
+
+    crearNotas();
 });
+const crearNotas =()=>{
+    notas = localStorage.getItem('notas');
+    if(notas == null){
+        arreglo= [];
+    }else{
+        arreglo = JSON.parse(notas);
+        
+    }
+    htmlnuevo = ""
+    arreglo.forEach((element, i )=>{
+        htmlnuevo = htmlnuevo+`
+            <div class="notaInd">
+                <h1>NOTA ${i+1} </h1>
+                <p>${element}</p>
+            </div> 
+        `
+    });
+    contenedor = document.getElementById("notas");
+    contenedor.innerHTML=htmlnuevo;
+}
